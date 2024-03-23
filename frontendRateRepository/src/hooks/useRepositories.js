36d2d02aef3702092a4cbfc12db77623da01273c
Jacob <jbale444@outlook.com>
2024-03-23 12:30:21 +0000

    Refactored code in to separate front and backend and updated application to connect to backend via HTTP connection to retrieve repositories from the backend for exercise 10.11

---
+++ frontendRateRepository/src/hooks/useRepositories.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    console.log("WORKING");
+
+
+    // Replace the IP address part with your own IP address!
+    const response = await fetch("http://192.168.1.140:5001/api/repositories");
+    const json = await response.json();
+
+    console.log(json);
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
